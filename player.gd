c45df9cbe1ced94c7eaa4221b6aed44177bf1caa
Dylan Sharkey <serghar12@gmail.com>
2026-08-23 20:09:34 -0700

    fix player movement input

--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var playerCamera = $Camera3D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -11,12 +12,15 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	# shift input by current camera angle for "smoothness"
+	input_dir = input_dir.rotated(playerCamera.rotation.x)
+	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
