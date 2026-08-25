59521e7b83f8896fa9987238c8471627c56a3e05
Dylan Sharkey <serghar12@gmail.com>
2026-08-25 15:21:06 -0700

    small missing old man updates

--- old_man.gd
+++ old_man.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+@onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -7,3 +8,5 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	move_and_slide()
+	
+	# TODO: Need to add a Area3D for detection and pass the body enter signal to the StateMachine function
