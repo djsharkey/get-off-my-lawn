d1de2bfc7c7645c4286918dec7200a0e0d497d01
Max Rose <maxrose93@hotmail.com>
2026-08-27 16:43:51 -0700

    starting script for adding colliders to grass for cutting

---
+++ scripts/grass_colliders.gd
@@ -0,0 +1,10 @@
+extends MultiMeshInstance3D
+
+@onready var tall_grass: MultiMeshInstance3D = $TallGrass
+
+func _ready() -> void:
+	if multimesh:
+		return
+		
+func cut_grass():
+	var mm: MultiMesh = tall_grass.multimesh
