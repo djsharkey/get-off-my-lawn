993e6aab77bd4986bbd4e1eea490961ad4c9d2c4
Dylan Sharkey <serghar12@gmail.com>
2026-08-26 21:38:43 -0700

    add some basic state functionality for the old man

--- old_man.gd
+++ old_man.gd
@@ -1,12 +1,25 @@
 extends CharacterBody3D
 
+@export var speed = 5.0
+
 @onready var animationPlayer = $AnimationPlayer
+@onready var stateMachine = $StateMachine
+
+var homeSpawnLocation
+var distractions = []
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	move_and_slide()
-	
-	# TODO: Need to add a Area3D for detection and pass the body enter signal to the StateMachine function
+
+func add_distraction(ref: Node3D):
+	if ref not in distractions:
+		distractions.append(ref)
+	if stateMachine.current_state.name != "InvestigateState":
+		stateMachine.transition_to_state("InvestigateState")
+
+func remove_distraction(ref: Node3D):
+	if ref in distractions:
+		distractions.erase(ref)
