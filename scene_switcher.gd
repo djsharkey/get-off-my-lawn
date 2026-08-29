26106c70949ec87798fcb8574dc4c08db4d554e2
Dylan Sharkey <serghar12@gmail.com>
2026-08-29 01:45:20 -0700

    Add main menu and game over on om collision

---
+++ scene_switcher.gd
@@ -0,0 +1,24 @@
+extends Node
+
+signal scene_changed()
+
+const SCENE_CHANGE_DELAY = 0.5
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var overlayColorRect = $Control/ColorRect
+
+func change_scene(path: String):
+	self.layer = 1
+	await get_tree().create_timer(SCENE_CHANGE_DELAY).timeout
+	
+	# Fade out
+	animationPlayer.play("fade")
+	await animationPlayer.animation_finished
+	
+	# Change level
+	get_tree().change_scene_to_file(path)
+	
+	# Fade in
+	animationPlayer.play_backwards("fade")
+	await animationPlayer.animation_finished
+	self.layer = -1
